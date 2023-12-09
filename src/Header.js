77f9e79c2396cbd2ff54c3f0e81c18da3139adb7
suraj7974 <surajpatelsuraj55@gmail.com>
2023-12-10 04:53:06 +0530

    new commit

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,38 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
   return (
     <div className="header">
-      <img className="header_logo" src="/images/goku.jpg" />
+      <img
+        className="header_logo"
+        src="https://wallpapercave.com/wp/wp7771255.png"
+      />
       <div className="header_search">
         <input className="header_searchInput" type="text" />
+        <SearchIcon className="header_searchIcon" />
       </div>
       <div className="header_nav">
         <div className="header_option">
-          <span className="header_option1">Hello guest1</span>
-          <span className="header_option2">Hello guest2</span>
+          <span className="header_option1">Hello guest</span>
+          <span className="header_option2">Sign In</span>
         </div>
 
         <div className="header_option">
-          <span className="header_option1">Hello guest3</span>
-          <span className="header_option2">Hello guest4</span>
+          <span className="header_option1">Returns</span>
+          <span className="header_option2">& Orders</span>
         </div>
 
         <div className="header_option">
-          <span className="header_option1">Hello guest5</span>
-          <span className="header_option2">Hello guest6</span>
+          <span className="header_option1"> Your</span>
+          <span className="header_option2"> Prime</span>
+        </div>
+
+        <div className="header_optionBasket">
+          <ShoppingBasketIcon />
+          <span className="herder_option2 header_basketCount">0</span>
         </div>
       </div>
     </div>
---
+++ src/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return <div className="home">i am home</div>;
+}
+
+export default Home;
