dae252b0a23aa47f038fe97f51d85b3c2dc05b71
suraj7974 <surajpatelsuraj55@gmail.com>
2023-12-10 02:00:09 +0530

    commit3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react"
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>
-        HELLO
-      </h1>
+      <Header />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
-    <div className='header'>
-        <img src='images/goku.jpg'/>
+    <div className="header">
+      <img className="header_logo" src="/images/goku.jpg" />
+      <div className="header_search">
+        <input className="header_searchInput" type="text" />
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_option1">Hello guest1</span>
+          <span className="header_option2">Hello guest2</span>
+        </div>
+
+        <div className="header_option">
+          <span className="header_option1">Hello guest3</span>
+          <span className="header_option2">Hello guest4</span>
+        </div>
+
+        <div className="header_option">
+          <span className="header_option1">Hello guest5</span>
+          <span className="header_option2">Hello guest6</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
