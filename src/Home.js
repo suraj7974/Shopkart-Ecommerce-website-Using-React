0c2561262d8debd443c8432a952516a96ae6b1c3
suraj7974 <surajpatelsuraj55@gmail.com>
2024-01-17 03:53:06 +0530

    home page product info,img

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,33 @@
 import React from "react";
 import "./Home.css";
+import Product from "./product";
 
 function Home() {
   return (
     <div className="home">
-       <div className="home__container"> 
-          <img src="https://m.media-amazon.com/images/I/71NqG9bBp7L._SX3000_.jpg" alt="" />
-         
-       </div>
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/I/71NqG9bBp7L._SX3000_.jpg"
+          alt=""
+        />
+
+        <div className="home__row">
+          <Product />
+          <Product />
+          
+        </div>
+        <div className="home__row">
+          {/* {product} */}
+          {/* {product} */}
+          {/* {product} */}
+        </div>
+        <div className="home__row">
+          {/* {product} */}
+       
+        </div>
+      </div>
     </div>
-      
   );
 }
 
---
+++ src/product.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./product.css";
+
+function product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p className="product__price">
+          <small> &#8377;</small>
+          <strong>69.69 </strong>
+        </p>
+        <div className="product__rating">
+          <p>&#9734;</p>
+          <p>&#9734;</p>
+          <p>&#9734;</p>
+        </div>
+      </div>
+       <img src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v1._SY304_CB573698005_.jpg" alt="" />
+    </div>
+  );
+}
+
+export default product;
