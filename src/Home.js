d7248bcef19de375d6f1d1d7ac1baaee9accb196
suraj7974 <surajpatelsuraj55@gmail.com>
2024-01-17 03:16:04 +0530

    lets start again

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <div className="home">i am home</div>;
+  return (
+    <div className="home">
+       <div className="home__container"> 
+          <img src="https://m.media-amazon.com/images/I/71NqG9bBp7L._SX3000_.jpg" alt="" />
+         
+       </div>
+    </div>
+      
+  );
 }
 
 export default Home;
