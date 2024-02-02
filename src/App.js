caed6da40848f066d3a1642bf43ebb16a086a957
suraj7974 <surajpatelsuraj55@gmail.com>
2024-02-03 02:50:59 +0530

    added a listner(useEffect) for authentication

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>>", authUser);
+
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     // BEM
     <Router>
       <div className="app">
-        
         <Routes>
-          <Route path="/checkout" element={[ <Header />, <Checkout />]} />
-          <Route path="/" element={[ <Header />, <Home />]} />
+          <Route path="/checkout" element={[<Header />, <Checkout />]} />
+          <Route path="/" element={[<Header />, <Home />]} />
           <Route path="/login" element={[<Login />]} />
-
         </Routes>
       </div>
     </Router>
