8aaaaa9b9febd0668b399700d241896f5df50c9b
suraj7974 <surajpatelsuraj55@gmail.com>
2024-02-03 02:54:10 +0530

    removed debugging logic for item if it is pushed in cart(because it is completed)

--- src/product.js
+++ src/product.js
@@ -4,7 +4,7 @@ import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
   const [{basket}, dispatch] = useStateValue();
-  console.log("this is the basket -->>", basket);
+ 
  
   const addToBasket = () => {
     dispatch({
