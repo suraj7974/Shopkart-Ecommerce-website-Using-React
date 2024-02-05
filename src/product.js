fabe3eee4d4407c4523570b927505b057e969298
suraj7974 <surajpatelsuraj55@gmail.com>
2024-02-05 09:17:48 +0530

    adding animation for product image sliding to cart

--- src/product.js
+++ src/product.js
@@ -1,17 +1,41 @@
 import React from "react";
 import "./product.css";
 import { useStateValue } from "./StateProvider";
+import _ from 'lodash';
 
 function Product({ id, title, image, price, rating }) {
-  const [{basket}, dispatch] = useStateValue();
- 
- 
+  const [{ basket }, dispatch] = useStateValue();
+  
+  var button = document.getElementsByTagName("button");
+  var isAnimating = false;
+
+  for (var but of button) {
+    but.addEventListener("click", (e) => {
+      if (!isAnimating) {
+        isAnimating = true;
+        e.target.disabled = true; // Disable the button during animation
+        var image = e.target.parentNode.querySelector("img");
+        var span = e.target.parentNode.querySelector("span");
+        var s_image = image.cloneNode(false);
+        span.appendChild(s_image);
+        span.classList.add("active");
+  
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.removeChild(s_image);
+          isAnimating = false;
+          e.target.disabled = false; // Enable the button after animation
+        }, 500);
+      }
+    });
+  }
+
   const addToBasket = () => {
     dispatch({
       type: "ADD_TO_BASKET",
       item: {
         id: id,
-        title: title, 
+        title: title,
         image: image,
         price: price,
         rating: rating,
@@ -35,7 +59,7 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
       <img src={image} alt="" />
-
+      <span className="hoverToCart"></span>
       <button onClick={addToBasket}>Add to basket</button>
     </div>
   );
