5fe21a0f4cf9a4bbd8a8916cf83f23695a4a7e6e
suraj7974 <surajpatelsuraj55@gmail.com>
2024-02-05 07:05:42 +0530

    deployment of the website

--- src/Checkout.js
+++ src/Checkout.js
@@ -9,6 +9,11 @@ function Checkout() {
   return (
     <div className="checkout">
       <div className="checkout__left">
+      <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
         <div>
           <h3>hello, {user? user.email : 'Guest'}</h3>
           <h2 className="checkout__title">Your Shopping Basket</h2>
