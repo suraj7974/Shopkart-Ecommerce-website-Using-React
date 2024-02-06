8b64bf9f8a461386eb81fa6d574adb123b487fe1
suraj7974 <surajpatelsuraj55@gmail.com>
2024-02-07 01:34:20 +0530

    added a sliding animation ans blur effect to the removing items in the cart

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,36 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CheckoutProduct.css";
 import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
   const [{ basket }, dispatch] = useStateValue();
-  const removeFromBasket = () => {
-    dispatch ({
-      type: 'REMOVE_FROM_BASKET',
-      id: id,
+  const [isRemoving, setIsRemoving] = useState(false);
 
-    })
+  const removeFromBasket = () => {
+    setIsRemoving(true);
+    setTimeout(() => {
+      dispatch({
+        type: "REMOVE_FROM_BASKET",
+        id: id,
+      });
+      setIsRemoving(false);
+    }, 1300);
   };
 
   return (
-    <div className="checkoutProduct">
-      <img className="checkoutProduct__image" src={image} alt="" />
-      <div className="checkoutProduct__info">
-        <p className="checkoutProduct__title">{title}</p>
-        <p className="checkoutProduct__price">
-          <small>$ </small>
-          <strong>{price}</strong>
-        </p>
-        <div className="checkoutProduct__rating">
-          {Array(rating)
-            .fill()
-            .map((_, i) => (
-              <p>&#9734;</p>
-            ))}
+    <div>
+      <div className={`checkoutProduct ${isRemoving ? "item-slide" : ""}`}>
+        <img className="checkoutProduct__image" src={image} alt="" />
+        <div className="checkoutProduct__info">
+          <p className="checkoutProduct__title">{title}</p>
+          <p className="checkoutProduct__price">
+            <small>$ </small>
+            <strong>{price}</strong>
+          </p>
+          <div className="checkoutProduct__rating">
+            {Array(rating)
+              .fill()
+              .map((_, i) => (
+                <p key={i}>&#9734;</p>
+              ))}
+          </div>
+          <button onClick={removeFromBasket}>Remove from Basket</button>
         </div>
-        <button onClick={removeFromBasket}>Remove from Basket</button>
       </div>
-    </div> 
+      <hr />
+    </div>
   );
 }
 
