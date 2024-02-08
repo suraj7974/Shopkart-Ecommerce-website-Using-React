971a33e9aa6ac66b0afd39815810468a3bd9cc9f
suraj7974 <surajpatelsuraj55@gmail.com>
2024-02-08 12:28:23 +0530

    added product section after proceed to checkout option

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,8 +3,10 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
+import { useNavigate } from "react-router-dom";
 
 function Subtotal() {
+  const navigate = useNavigate();
   const [{ basket }, dispatch] = useStateValue();
    return (
     <div className="Subtotal">
@@ -26,7 +28,7 @@ function Subtotal() {
         thousandsSeparator={true}
         prefix={"$"}
       />
-      <button>Proceed to checkout</button>
+      <button onClick={e => navigate('/payment')}>Proceed to checkout</button>
     </div>
   );
 }
