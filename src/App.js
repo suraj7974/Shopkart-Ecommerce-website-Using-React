db92df0a8278c605f78340f1c8d1d521ecc4b905
suraj7974 <surajpatelsuraj55@gmail.com>
2024-02-08 12:25:59 +0530

    added payment page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Checkout from "./Checkout";
 import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from "./Payment";
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -37,6 +38,7 @@ function App() {
           <Route path="/checkout" element={[<Header />, <Checkout />]} />
           <Route path="/" element={[<Header />, <Home />]} />
           <Route path="/login" element={[<Login />]} />
+          <Route path="/payment" element={[<Header />, <Payment />]} />
         </Routes>
       </div>
     </Router>
